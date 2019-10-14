e9598cab70dfbc3b9247aff2732b9fdc5d206ee9
Taran <thetaranneeld@gmail.com>
2019-10-14 11:56:28 -0600

    not finished

--- index.js
+++ index.js
@@ -30,19 +30,22 @@ server.post('/api/register', (req, res) => {
 
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
-
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'You cannot pass!!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({ message: 'please provide credentials' });
+  }
 });
 
 server.get('/api/users', (req, res) => {
@@ -57,6 +60,14 @@ server.get('/hash', (req, res) => {
   let password = req.header;
   let hash = bcrypt.hashSync(password)
 })
+
+function protected() {
+  let password = req.header.password;
+  let username = req.header.username;
+  if (username ) {
+
+  }
+}
 
 
 const port = process.env.PORT || 5000;
